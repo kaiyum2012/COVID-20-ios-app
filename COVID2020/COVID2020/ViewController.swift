025aad552af7fa90603cc7c41d144bd9a48aa626
Abdulkaiyum Shaikh <abdulkaiyum.creatrix>
2020-04-15 19:15:40 -0400

    Fresh xcode project created

---
+++ COVID2020/COVID2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID2020
+//
+//  Created by Student on 2020-04-15.
+//  Copyright © 2020 Kaiyum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
